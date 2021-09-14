eb08dbb3aeb6583ff9149daf1a6b7e7bd4bc68e1
izthemann92 <82607762+izthemann92@users.noreply.github.com>
2021-09-14 15:33:53 -0500

    WORK COMPLETED

--- homework 1/main.py
+++ homework 1/main.py
@@ -3,7 +3,7 @@ print('You entered:', user_num)
 num2 = user_num**2
 print(user_num,'squared is',num2)
 num3 = user_num**3
-print('\nAnd',user_num,'cubed is',num3,'!!\n')
+print('And',user_num,'cubed is',num3,'!!')
 num4 = int(input('Enter another integer:\n'))
-print(user_num,'+',num4,'is',user_num+num4,'\n')
+print(user_num,'+',num4,'is',user_num+num4)
 print(user_num,'*',num4,'is',user_num*num4)
