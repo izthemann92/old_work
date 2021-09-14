c8440f6a90888912cffe3ff3e59214459fde5e8f
izthemann92 <82607762+izthemann92@users.noreply.github.com>
2021-09-14 15:26:39 -0500

    started working on zylabs 1.20 and just needed to take a break

--- homework 1/main.py
+++ homework 1/main.py
@@ -1,5 +1,5 @@
-user_num = int(input('Enter integer: \n'))
-print('you entered:\n', user_num)
+user_num = int(input('Enter integer:\n'))
+print('you entered:', user_num)
 num2 = user_num**2
 print(user_num,'squared is',num2)
 num3 = user_num**3
