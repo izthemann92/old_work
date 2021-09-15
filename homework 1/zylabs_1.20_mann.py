a5512cbd3fe6ea1bf77188fa644259ffea35d43b
izthemann92 <82607762+izthemann92@users.noreply.github.com>
2021-09-14 20:06:43 -0500

    started working on zylabs 1.20 and just needed to take a break

--- homework 1/zylabs_1.20_mann.py
+++ homework 1/zylabs_1.20_mann.py
@@ -1,10 +1,11 @@
-
+#Blake Mann
+#psid:1832387
 user_num = int(input('Enter integer:\n'))
 print('You entered:', user_num)
 num2 = user_num**2
-print(user_num,'squared is',num2)
+print(user_num, 'squared is', num2)
 num3 = user_num**3
-print('And',user_num,'cubed is',num3,'!!')
+print('And', user_num, 'cubed is', num3, '!!')
 num4 = int(input('Enter another integer:\n'))
-print(user_num,'+',num4,'is',user_num+num4)
-print(user_num,'*',num4,'is',user_num*num4)
+print(user_num, '+', num4, 'is', user_num+num4)
+print(user_num, '*', num4, 'is', user_num*num4)
