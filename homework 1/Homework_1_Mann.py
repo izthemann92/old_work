ce7cb6da53f090446f19392a776c2091cfb0457e
izthemann92 <82607762+izthemann92@users.noreply.github.com>
2021-09-16 19:59:15 -0500

    finished coding all assignments

--- homework 1/Homework_1_Mann.py
+++ homework 1/Homework_1_Mann.py
@@ -1,7 +1,20 @@
 # Blake Mann
 # PSID: 1832387
 print("Birthday Calculator")
-x, y, z = input("Enter current day").split()
-print("Month:", x)
-print("Day:", y)
-print("Year", z)
+month = int(input("Month: "))
+Day = int(input("Day: "))
+year = int(input("Year: "))
+print("\nBirthday")
+bmonth = int(input("Month: "))
+bday = int(input("Day: "))
+byear = int(input("Year: "))
+
+age = year - byear
+if (month == bmonth) and (Day == bday):
+    print("Happy Birthday!!!")
+    print("You are", age, "old")
+elif month > bmonth and Day >= bday:
+    age = age - 1
+    print("You are", age, "years old")
+elif month <= bmonth and Day >= bday:
+    print("You are", age, "old")
