d09811457ad325856c8a9d9d5574639a2f9ba883
izthemann92 <82607762+izthemann92@users.noreply.github.com>
2021-10-12 16:07:25 -0500

    playing

--- Homework 2/Homework_2_Mann.py
+++
@@ -1,8 +0,0 @@
-# Name: Blake Mann
-# PSID: 1832387
-
-number = 0
-
-while number != -1:
- number = int(input('TELL ME YOUR NUMBER, -1 TO EXIT'))
- print(number)
---
+++ lectures/week 6/lecture_6_practice.py
@@ -0,0 +1,4 @@
+# lecture 6 practice
+my_file = open('data1(1).txt', 'r')
+file_contents = my_file.read()
+print(file_contents)
