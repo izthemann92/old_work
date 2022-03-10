895d1d85c344edd37e47038afa89aed5ef08e672
izthemann92 <82607762+izthemann92@users.noreply.github.com>
2022-03-09 20:29:45 -0600

    homework completed

--- Homework 2/Homework_2_Mann.py
+++ Homework 2/Homework_2_Mann.py
@@ -2,7 +2,7 @@
 # PSID: 1832387
 import datetime
 import time
-months = {"January": "01", "February": "02", "March": "03", "April": "04", "May": "05", "June": "06", "July": "07", "August": "08",
+months = {"January": "01", "February": "02", "March": "03", "April": "04", "May": "05", "June": "06", "July": "07", "August" : "08",
           "September": "09", "October": "10", "November": "11", "December": "12"}
 
 
